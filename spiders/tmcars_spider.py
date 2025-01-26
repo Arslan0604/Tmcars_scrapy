5b4e3fc4a09cb5579c9b4444da90fa91a1a4b82f
Arslan Ovezov <arslan.datascience@gmail.com>
2025-01-26 20:48:18 +0500

    ars2

--- spiders/tmcars_spider.py
+++ spiders/tmcars_spider.py
@@ -8,3 +8,9 @@ class TmcarsSpiderSpider(scrapy.Spider):
 
     def parse(self, response):
         pass
+        toyota = response.css('div.col-lg-6.col-md-6.col-xl-4.col-sm-6.col-xs-12')
+
+        city = response.css('p.pb-0.pt-0.mb-2.mt-1.no-wrapped::text').get()
+        title = response.css('h5.font-weight-semibold.mt-1.shorted-title::text').get()
+        price = response.css('span.font-weight-bold::text').get()
+        
